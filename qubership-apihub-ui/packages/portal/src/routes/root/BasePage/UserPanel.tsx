7885743380d975501b8903aaea95ce302fc4d661
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2025-02-28 16:34:57 +0400

    Added menu item "My Profile"

--- qubership-apihub-ui/packages/portal/src/routes/root/BasePage/UserPanel.tsx
+++ qubership-apihub-ui/packages/portal/src/routes/root/BasePage/UserPanel.tsx
@@ -14,16 +14,19 @@
  * limitations under the License.
  */
 
-import type { FC } from 'react'
-import { memo } from 'react'
+import { getProfilePath } from '@apihub/routes/NavigationProvider'
 import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined'
 import { Avatar, IconButton, MenuItem } from '@mui/material'
-import { useAuthorization } from '@netcracker/qubership-apihub-ui-shared/hooks/authorization'
-import { UserAvatar } from '@netcracker/qubership-apihub-ui-shared/components/Users/UserAvatar'
 import { MenuButton } from '@netcracker/qubership-apihub-ui-shared/components/Buttons/MenuButton'
+import { UserAvatar } from '@netcracker/qubership-apihub-ui-shared/components/Users/UserAvatar'
+import { useAuthorization } from '@netcracker/qubership-apihub-ui-shared/hooks/authorization'
+import type { FC } from 'react'
+import { memo } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export const UserPanel: FC = memo(() => {
   const [authorization, , removeAuthorization] = useAuthorization()
+  const navigate = useNavigate()
 
   return (
     <>
@@ -43,6 +46,14 @@ export const UserPanel: FC = memo(() => {
         icon={<KeyboardArrowDownOutlinedIcon/>}
         data-testid="UserMenuButton"
       >
+        <MenuItem
+          data-testid="ProfileMenuItem"
+          onClick={() => {
+            navigate(getProfilePath())
+          }}
+        >
+          My Profile
+        </MenuItem>
         <MenuItem
           data-testid="LogoutMenuItem"
           onClick={() => {
