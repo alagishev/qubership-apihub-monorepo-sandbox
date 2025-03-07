c4b257253b82d5b088339526763c19704b288b19
b41ex <bochencev.qubership@gmail.com>
2025-03-07 15:49:41 +0300

    fix: revert unintended changes
    This reverts commit 1e263148048c1d7d1caf1dbd96595561e70d214f.

--- qubership-apihub-api-processor/src/components/compare/compare.operations.ts
+++ qubership-apihub-api-processor/src/components/compare/compare.operations.ts
@@ -38,7 +38,6 @@ import {
   OperationIdentityMap,
   takeSubstringIf,
   totalChanges,
-  getNormalizedOperationId,
 } from './compare.utils'
 import {
   calculateApiAudienceTransitions,
@@ -350,6 +349,7 @@ const HANDLE_TYPE_FULLY_REMOVED = 'removed'
 type HandleType = typeof HANDLE_TYPE_FULLY_ADDED | typeof HANDLE_TYPE_FULLY_REMOVED
 
 const createPairOperationsMap = (currGroupSlug: string, prevGroupSlug: string, currentOperations: ResolvedOperation[], previousOperations: ResolvedOperation[]): Record<string, { previous?: ResolvedOperation; current: ResolvedOperation }> => {
+
   const operationsMap: Record<string, { previous?: ResolvedOperation; current: ResolvedOperation }> = {}
 
   for (const currentOperation of currentOperations) {
