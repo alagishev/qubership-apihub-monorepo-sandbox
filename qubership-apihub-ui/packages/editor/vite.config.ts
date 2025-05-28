68e5d016dd3f89c046cbcd8f1a77cfeade535b1f
Pavel Makeev <pavel.makeev.qubership@gmail.com>
2025-05-28 14:20:45 +0400

    feat: configure HTTP caching for the UI (#89)
    * chore: improving http caching
    
    * fix: add monaco hash
    
    * fix: clean
    
    * fix: reducing the max age to a week
    
    * fix: Edit list of files for caching
    
    * fix: del create-hash

--- qubership-apihub-ui/packages/editor/vite.config.ts
+++ qubership-apihub-ui/packages/editor/vite.config.ts
@@ -24,6 +24,7 @@ import copy from 'rollup-plugin-copy'
 import ignoreDotsOnDevServer from 'vite-plugin-rewrite-all'
 import { VitePluginFonts } from 'vite-plugin-fonts'
 import { visualizer as bundleVisualizer } from 'rollup-plugin-visualizer'
+import monacoWorkerHashPlugin from '../../vite-monaco-worker-hash'
 
 const proxyServer = ''
 const devServer = 'http://localhost:3004'
@@ -51,6 +52,7 @@ export default defineConfig(({ mode }) => {
           entry: 'monaco-graphql/dist/graphql.worker',
         }],
       }),
+      monacoWorkerHashPlugin({monacoDir: 'dist/monacoeditorwork', htmlPath: 'dist/index.html'}),
       copy({
         targets: [
           {
--- qubership-apihub-ui/packages/portal/vite.config.ts
+++ qubership-apihub-ui/packages/portal/vite.config.ts
@@ -9,6 +9,7 @@ import ignoreDotsOnDevServer from 'vite-plugin-rewrite-all'
 import { VitePluginFonts } from 'vite-plugin-fonts'
 import { visualizer as bundleVisualizer } from 'rollup-plugin-visualizer'
 import inject from '@rollup/plugin-inject'
+import monacoWorkerHashPlugin from '../../vite-monaco-worker-hash'
 
 const proxyServer = ''
 const devServer = 'http://localhost:3003'
@@ -32,6 +33,7 @@ export default defineConfig(({ mode }) => {
           entry: 'monaco-graphql/dist/graphql.worker',
         }],
       }),
+      monacoWorkerHashPlugin({monacoDir: 'dist/monacoeditorwork', htmlPath: 'dist/index.html'}),
       copy({
         targets: [
           {
