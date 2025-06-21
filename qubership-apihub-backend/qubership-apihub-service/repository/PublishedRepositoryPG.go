e16e615b6887c429e4d5b748f313a15e4a1f2a0c
Viacheslav Lunev <viacheslav.lunev.qubership@gmail.com>
2025-06-21 12:47:32 +0300

    Fix grouped operations corruption during migration. (#133)

--- qubership-apihub-backend/qubership-apihub-service/repository/PublishedRepositoryPG.go
+++ qubership-apihub-backend/qubership-apihub-service/repository/PublishedRepositoryPG.go
@@ -1121,9 +1121,22 @@ func (p publishedRepositoryImpl) CreateVersionWithData(packageInfo view.PackageI
 			}
 			utils.PerfLog(time.Since(start).Milliseconds(), 50, "CreateVersionWithData: operationsData insert")
 		}
+
+		var existingGroupedOperations []entity.GroupedOperationEntity
+
 		if packageInfo.MigrationBuild {
 			// In case of migration list of operations may change due to new builder implementation, so need to cleanup existing list before insert
+
 			start = time.Now()
+			// Need to preserve grouped operations, since it will be deleted along with operations
+			err = tx.Model(&existingGroupedOperations).
+				Where("package_id = ?", version.PackageId).
+				Where("version = ?", version.Version).
+				Where("revision = ?", version.Revision).Select()
+			if err != nil {
+				return fmt.Errorf("failed to fetch grouped operations before operations cleanup: %w", err)
+			}
+
 			_, err := tx.Model(&entity.OperationEntity{}).
 				Where("package_id=?", version.PackageId).
 				Where("version=?", version.Version).
@@ -1226,6 +1239,33 @@ func (p publishedRepositoryImpl) CreateVersionWithData(packageInfo view.PackageI
 			}
 			utils.PerfLog(time.Since(start).Milliseconds(), 50, "CreateVersionWithData: builderNotifications insert")
 		}
+
+		if len(existingGroupedOperations) > 0 {
+			// Restore grouped operations
+			start = time.Now()
+
+			// Check that every grouped operation still exists in the new list
+			opMap := make(map[string]struct{})
+			for _, op := range operations {
+				opMap[op.OperationId] = struct{}{}
+			}
+			var groupedOperationsToRestore []entity.GroupedOperationEntity
+			for _, groupedOperation := range existingGroupedOperations {
+				if _, ok := opMap[groupedOperation.OperationId]; ok {
+					groupedOperationsToRestore = append(groupedOperationsToRestore, groupedOperation)
+				} else {
+					log.Warnf("Grouped operation with id %s is not found in the operations list and will not be restored. PackageId=%s, version=%s, revision=%d",
+						groupedOperation.OperationId, version.PackageId, version.Version, version.Revision)
+				}
+			}
+
+			_, err = tx.Model(&groupedOperationsToRestore).Insert()
+			if err != nil {
+				return fmt.Errorf("failed to restore grouped operations: %w", err)
+			}
+			utils.PerfLog(time.Since(start).Milliseconds(), 50, "CreateVersionWithData: restore grouped operations")
+		}
+
 		if !packageInfo.MigrationBuild {
 			start = time.Now()
 			err = p.propagatePreviousOperationGroups(tx, version)
