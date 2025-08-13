800815be274431b11fd3bedc97698dba9c4b76ac
Aleksandr Agishev <aleksandr.agishev.qubership@gmail.com>
2025-08-13 09:43:41 +0300

    feat: adding api-linter to nginx config (#133)
    * [FE]Configure Monaco bundling during REST playground build
    
    * fixed build
    
    * fixed build 2
    
    * fixed build 2
    
    * ready
    
    * fixed nginx.conf.template
    
    * fixed nginx.conf.template
    
    * feat: add api-linter add-on section & make runtime resolving of extensions URLs
    
    feat: make resolve of nc-service be on runtime, not on startup - required to handle case if nc-service is absent
    
    feat: add dynamic set of resolver
    
    fixup
    
    ---------
    
    Co-authored-by: Iurii Golovinskii <iurii-golovinskii.qubership@gmail.com>

--- qubership-apihub-ui/nginx/entrypoint.sh
+++ qubership-apihub-ui/nginx/entrypoint.sh
@@ -5,6 +5,9 @@ if ! whoami &> /dev/null; then
   fi
 fi
 
+DNS_RESOLVERS="$(awk '/^nameserver/{print $2}' /etc/resolv.conf | paste -sd' ' -)"
+export DNS_RESOLVERS
 
-envsubst '${APIHUB_BACKEND_ADDRESS} ${APIHUB_NC_SERVICE_ADDRESS}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+envsubst '${APIHUB_BACKEND_ADDRESS} ${APIHUB_NC_SERVICE_ADDRESS} ${API_LINTER_SERVICE_ADDRESS} ${DNS_RESOLVERS}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 nginx -g "daemon off;"
